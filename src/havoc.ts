12b36dab424abd970b7b2c97e9077f6bd0ad3429
Luc Gagan <lucgagan@gmail.com>
2023-11-11 18:30:36 -0600

    feat: randomize delay

--- src/havoc.ts
+++ src/havoc.ts
@@ -1,9 +1,13 @@
 import { Page } from "@playwright/test";
 import { setTimeout } from "timers/promises";
 
+// utility for generating a random number between min and max
+const random = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
 export const havoc = async (page: Page) => {
   await page.route("**/*", async (route) => {
-    await setTimeout(500);
+    await setTimeout(random(500, 1000));
     await route.continue();
   });
 };
